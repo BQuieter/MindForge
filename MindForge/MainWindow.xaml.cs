a12eaad969aa399f5ad7671ccf57e4b962ff832e
Dima Boryaev <dimaboryaev1@gmail.com>
2024-09-29 12:39:14 +0300

    Maximazed works

--- MindForge/MainWindow.xaml.cs
+++ MindForge/MainWindow.xaml.cs
@@ -4,21 +4,66 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 
 namespace MindForge
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoadingWindow : Window
     {
-        public MainWindow()
+        private Mutex mutex;
+        private string mutexName = "MyApplicationMutex";
+        public LoadingWindow()
         {
+            mutex = new Mutex(false, mutexName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                WindowHelper.MaximizeWindow("MindForge");
+                Application.Current.Shutdown();
+                return;
+            }
             InitializeComponent();
+            this.BorderThickness = SystemParametersFix.WindowResizeBorderThickness;
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+                this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void MindForge_Closed(object sender, EventArgs e)
+        {
+            mutex.ReleaseMutex();
         }
     }
 }
---
+++ MindForge/WindowHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public static class WindowHelper
+{
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    public static void MaximizeWindow(string windowTitle)
+    {
+        IntPtr hWnd = FindWindow(null, windowTitle);
+
+        if (hWnd != IntPtr.Zero)
+        {
+            ShowWindow(hWnd, 1);
+            SetForegroundWindow(hWnd);
+        }
+    }
+}
