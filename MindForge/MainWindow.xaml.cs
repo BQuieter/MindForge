43b444399580b70c0606c91d45d0c18951eff99c
Dima Boryaev <dimaboryaev1@gmail.com>
2024-10-02 21:33:38 +0300

    try to change

--- MindForge/MainWindow.xaml.cs
+++ MindForge/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace MindForge
     /// </summary>
     public partial class LoadingWindow : Window
     {
-        private readonly string regexLogin = @"^[a-zA-Z0-9-_@.]*$";
+        private readonly string regexLogin = @"^[а-яА-Яa-zA-Z0-9-_@.]*$";
         private readonly string regexPassword = @"^[a-zA-Z0-9-_!@#$&.,]*$";
 
         private Mutex mutex;
@@ -40,14 +40,13 @@ namespace MindForge
             }
             InitializeComponent();
             this.BorderThickness = SystemParametersFix.WindowResizeBorderThickness;
-            /////////////////////////////////////////////////////////
-            
-            /*Dispatcher.InvokeAsync(async () => 
+            ///////////////////////// Когда будет сервак, переделай
+            Dispatcher.InvokeAsync(async () => 
             {
-                await Task.Delay(2000);
+                await Task.Delay(1500);
                 LoadingGrid.Visibility = Visibility.Collapsed;
-                AuthorizationGrid.Visibility = Visibility.Visible;
-            });*/
+                SignInGrid.Visibility = Visibility.Visible;
+            });
         }
 
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
@@ -56,15 +55,11 @@ namespace MindForge
                 this.DragMove();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click(object sender, RoutedEventArgs e) =>
             this.Close();
-        }
 
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) =>
                 this.WindowState = WindowState.Minimized;
-        }
 
         private void MaximizeButton_Click(object sender, RoutedEventArgs e)
         {
@@ -79,79 +74,110 @@ namespace MindForge
             mutex.ReleaseMutex();
 
         private void LoginBox_GotFocus(object sender, RoutedEventArgs e) =>
-            TextBoxTextHelp((TextBox)sender, "Логин или email", "", new SolidColorBrush(Color.FromRgb(160, 160, 160)));
-
+            ChangeWatermarkVisible(sender, LoginBlock, Visibility.Collapsed);
         private void LoginBox_LostFocus(object sender, RoutedEventArgs e) =>
-            TextBoxTextHelp((TextBox)sender, "", "Логин или email", new SolidColorBrush(Color.FromRgb(112, 112, 112)));
-        private void TextBoxTextHelp(TextBox textBox, string expectedText, string changeText, SolidColorBrush brush)
-        {
-            if (textBox.Text == expectedText)
-            {
-                textBox.Text = changeText;
-                textBox.Foreground = brush;
-            }
-        }
-
+            ChangeWatermarkVisible(sender, LoginBlock, Visibility.Visible);
         private void PasswordBox_GotFocus(object sender, RoutedEventArgs e) =>
-            TextBoxTextHelp((PasswordBox)sender, "*****", "", new SolidColorBrush(Color.FromRgb(160, 160, 160)));
-
+            ChangeWatermarkVisible(sender, PasswordBlock, Visibility.Collapsed);
         private void PasswordBox_LostFocus(object sender, RoutedEventArgs e) =>
-            TextBoxTextHelp((PasswordBox) sender, "", "*****", new SolidColorBrush(Color.FromRgb(160, 160, 160)));
-
-        private void TextBoxTextHelp(PasswordBox textBox, string expectedText, string changeText, SolidColorBrush brush)
+            ChangeWatermarkVisible(sender, PasswordBlock, Visibility.Visible);
+        private void RegistrationLoginBox_GotFocus(object sender, RoutedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationLoginBlock, Visibility.Collapsed);
+        private void RegistrationLoginBox_LostFocus(object sender, RoutedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationLoginBlock, Visibility.Visible);
+        private void RegistrationEmailBox_GotFocus(object sender, RoutedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationEmailBlock, Visibility.Collapsed);
+        private void RegistrationEmailBox_LostFocus(object sender, RoutedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationEmailBlock, Visibility.Visible);
+        private void RegistrationPasswordBox_GotFocus(object sender, RoutedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationPasswordBlock, Visibility.Collapsed);
+        private void RegistrationPasswordBox_LostFocus(object sender, RoutedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationPasswordBlock, Visibility.Visible);
+        private void LoginBox_TextChanged(object sender, TextChangedEventArgs e) =>
+            ChangeWatermarkVisible(sender, LoginBlock);
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e) =>
+            ChangeWatermarkVisible(sender, PasswordBlock);
+        private void RegistrationLoginBox_TextChanged(object sender, TextChangedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationLoginBlock);
+        private void RegistrationEmailBox_TextChanged(object sender, TextChangedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationEmailBlock);
+        private void RegistrationPasswordBox_TextChanged(object sender, TextChangedEventArgs e) =>
+            ChangeWatermarkVisible(sender, RegistrationPasswordBlock);
+        private void ChangeWatermarkVisible(object sender,TextBlock textBlock, Visibility visible)
         {
-            if (textBox.Password == expectedText)
+            if (visible == Visibility.Visible)
             {
-                textBox.Password = changeText;
-                textBox.Foreground = brush;
+                bool isEmpty = false;
+                if (sender is TextBox)
+                {
+                    TextBox textBox = (TextBox)sender;
+                    isEmpty = textBox.Text.Length == 0;
+                }
+                else if(sender is PasswordBox)
+                {
+                    PasswordBox passwordBox = (PasswordBox)sender;
+                    isEmpty = passwordBox.Password.Length == 0;
+                }
+                if (isEmpty)
+                    textBlock.Visibility = visible;
             }
+            else
+                textBlock.Visibility = visible;
         }
-
+        private void ChangeWatermarkVisible(object sender, TextBlock textBlock)
+        {
+            bool isEmpty = false;
+            if (sender is TextBox)
+            {
+                TextBox textBox = (TextBox)sender;
+                isEmpty = textBox.Text.Length == 0;
+            }
+            else if (sender is PasswordBox)
+            {
+                PasswordBox passwordBox = (PasswordBox)sender;
+                isEmpty = passwordBox.Password.Length == 0;
+            }
+            textBlock.Visibility = isEmpty ? Visibility.Visible : Visibility.Collapsed;
+        }
         private void EnterKeyFocus(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
                 MainGrid.Focus();
         }
-
-
         private void LoseFocus(object sender, MouseButtonEventArgs e) =>
             MainGrid.Focus();
 
         private void DisableContextMenu(object sender, MouseButtonEventArgs e) =>
             e.Handled = true;
 
-        private void LoginBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void GoToRegisterButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            TextBox textBox = (TextBox)sender;
-            string text =  textBox.Text;
-            bool isMatch = Regex.IsMatch(textBox.Text, regexLogin);
-            if (!isMatch && text.Length > 1 && text != "Логин или email")
-            {
-                textBox.Text = text.Substring(0, text.Length - 1);
-                textBox.SelectionStart = text.Length-1;
-            }
-            else if (!isMatch && (text.Length == 1 || text.Length == 0))
-                textBox.Text = "";
+            SignInGrid.Visibility = Visibility.Collapsed;
+            RegistrationGrid.Visibility = Visibility.Visible;
         }
 
-        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        private void GoToSignInButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
-            PasswordBox passwordBox = (PasswordBox)sender;
-            string text = passwordBox.Password;
-            bool isMatch = Regex.IsMatch(passwordBox.Password, regexPassword);
-
-            if (!isMatch && text.Length > 1 && text != "*****")
-            {
-                passwordBox.Password = text.Substring(0, text.Length - 1);
-                SetSelection(passwordBox, text.Length - 1, 0);
-            }
-            else if (!isMatch && (text.Length == 1 || text.Length == 0))
-                passwordBox.Password = "";
+            RegistrationGrid.Visibility = Visibility.Collapsed;
+            SignInGrid.Visibility = Visibility.Visible;
         }
-        private void SetSelection(PasswordBox passwordBox, int start, int length)
+
+        private void ResetValues(object sender, DependencyPropertyChangedEventArgs e)
         {
-            passwordBox.GetType().GetMethod("Select", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(passwordBox, new object[] { start, length });
+            Grid grid = (Grid)sender;
+            foreach(var a in grid.Children)
+            {
+                if (a is TextBox)
+                {
+                    TextBox textBox = (TextBox)a;
+                    textBox.Text = "";
+                }
+                else if (a is PasswordBox)
+                {
+                    PasswordBox passwordBox = (PasswordBox)a;
+                    passwordBox.Password = "";
+                }    
+            }
         }
     }
 }
